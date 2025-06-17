db8d9e2eb1b8a45bd985d0b817ec7e00550397f8
Macarat2009 <mat.becvar@gmail.com>
2025-06-17 15:49:48 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('Script loaded');
+
+document.querySelectorAll('.navbar a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    
+    this.classList.add('aktivni');
+    setTimeout(() => {
+      window.location.href = this.href;
+    }, 100);
+  });
+});
